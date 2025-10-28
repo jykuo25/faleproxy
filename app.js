85a56110911c8b3d60d76ea63a93abe4744d7c28
Jodie Kuo <jodie_kuo@college.harvard.edu>
2025-10-28 19:33:17 -0400

    support FALE in text nodes

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post('/fetch', async (req, res) => {
     }).each(function() {
       // Replace text content but not in URLs or attributes
       const text = $(this).text();
-      const newText = text.replace(/Yale/g, 'Fale').replace(/yale/g, 'fale');
+      const newText = text.replace(/Yale/g, 'Fale').replace(/yale/g, 'fale').replace(/YALE/g, 'FALE');
       if (text !== newText) {
         $(this).replaceWith(newText);
       }
